41e50c2ce91cc853432490c40f420439afb43252
Christian Svensson <blue@cmd.nu>
2020-12-06 14:04:19 +0100

    Ansible skeleton

---
+++ collection/plugins/modules/boot.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+# TODO: describe
+# Copyright 2020 Tateru Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+DOCUMENTATION = r'''
+---
+module: boot
+
+short_description: Tateru installer boot-up module
+version_added: "0.0.1"
+
+description: Tateru boot module is used to start up a machine into the Tateru deployment system.
+
+options:
+    machine:
+        description: The machine name to boot up into the Tateru installer.
+        required: true
+        type: str
+    ssh_pub_key:
+        description: The SSH public key to whitelist for SSH access into the installer live environment.
+        required: true
+        type: str
+extends_documentation_fragment:
+    - tateru.deploy.boot
+author:
+    - Tateru Authors
+'''
+
+EXAMPLES = r'''
+# Provision the machine test1
+- name: Provision test1
+  tateru.deploy.boot:
+    machine: test1
+    ssh_pub_key: "ssh-ed25519 AAA[...]AB user@laptop"
+'''
+
+from ansible.module_utils.basic import AnsibleModule
+
+
+def run_module():
+    module_args = dict(
+        machine=dict(type='str', required=True),
+        ssh_pub_key=dict(type='str', required=True),
+    )
+
+    result = dict(
+        changed=False,
+    )
+    module = AnsibleModule(
+        argument_spec=module_args,
+        supports_check_mode=True
+    )
+
+    if module.check_mode:
+        module.exit_json(**result)
+
+    module.exit_json(**result)
+
+
+def main():
+    run_module()
+
+
+if __name__ == '__main__':
+    main()
---
+++ collection/plugins/modules/installer_address.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+# TODO: describe
+# Copyright 2020 Tateru Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+DOCUMENTATION = r'''
+---
+module: boot
+
+short_description: Tateru installer address finder mopdule
+version_added: "0.0.1"
+
+description: Tateru installer address module is used to find the address to the Tateru installer instance running on a given machine
+
+options:
+    machine:
+        description: The machine name lookup.
+        required: true
+        type: str
+extends_documentation_fragment:
+    - tateru.deploy.installer_address
+author:
+    - Tateru Authors
+'''
+
+EXAMPLES = r'''
+# Find address of the installer running at test1
+- name: Wait for installer address for test1
+  tateru.deploy.installer_address:
+    machine: test1
+  register: installer_address
+'''
+
+RETURN = r'''
+address:
+    description: The ephemeral address the installer is reachable by.
+    type: str
+    returned: always
+    sample: '2001:0db8:85a3::8a2e:0370:7334'
+'''
+
+from ansible.module_utils.basic import AnsibleModule
+import time
+
+
+def run_module():
+    module_args = dict(
+        machine=dict(type='str', required=True),
+    )
+
+    result = dict(
+        changed=False,
+        address='::'
+    )
+    module = AnsibleModule(
+        argument_spec=module_args,
+        supports_check_mode=True
+    )
+
+    if module.check_mode:
+        module.exit_json(**result)
+
+    result['address'] = '::1'
+    # TODO: Fake wait to demo flow
+    time.sleep(3)
+    module.exit_json(**result)
+
+
+def main():
+    run_module()
+
+
+if __name__ == '__main__':
+    main()
